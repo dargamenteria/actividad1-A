e9c505717abfee45b8cca197dd8e04bbddb5795f
dargamenteria <dargamenteria@paranoidworld.es>
2024-05-02 21:01:04 +0200

    Addded code and test

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -16,6 +16,28 @@ class TestApi(unittest.TestCase):
         self.assertIsNotNone(BASE_URL, "URL no configurada")
         self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
 
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/4/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(response.read().decode(), "2.0", "ERROR DIVIDE")
+
+    def test_api_divide_by_zero(self):
+        with pytest.raises(Exception) as e:
+            url = f"{BASE_URL}/calc/divide/4/0"
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        assert str(e.value) == "HTTP Error 406: NOT ACCEPTABLE"
+
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/1/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(response.read().decode(), "2", "ERROR MULTIPLY")
+
     def test_api_add(self):
         url = f"{BASE_URL}/calc/add/1/2"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
